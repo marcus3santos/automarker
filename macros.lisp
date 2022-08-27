9ad821c80b9563d85740bdaa8cd761ca03f1ca41
Marcus Santos <34948880+marcus3santos@users.noreply.github.com>
2022-08-27 14:46:17 -0400

    Add files via upload

---
+++ macros.lisp
@@ -0,0 +1,61 @@
+(in-package :automarker)
+
+(defun report-result (result form)
+  (let ((res (not (or (eq result 'runtime-error)
+		      (typep result 'condition)
+		      (not result)))))
+    (push (list res result (car *test-name*) form) *results*)
+    ;; (format t "~:[FAIL~;pass~] ...~a: ~a~%" result *test-name* form)
+    res))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+;; Added to handle detection of endless loop when evaluating test cases
+(defmacro time-execution  (expr maxtime)
+  "Evaluates expr in a separate thread. 
+   If expr's execution time reaches maxtime seconds, then kills the thread and
+   pushes the expression that ran out of execution time in *RUNTIME-ERROR*
+   then returns *RUNTIME-ERROR*. Otherwise returns the result of evaluating expr."
+  (let ((thread (gensym))
+	(keep-time (gensym))
+	(stime (gensym))
+	(res (gensym)))
+    `(let* ((,res nil)
+	    (,thread (sb-thread:make-thread 
+		     (lambda () (setf ,res ,expr)))))
+       (labels ((,keep-time (,stime)
+		  (cond ((and (> (/ (- (get-internal-real-time) ,stime) 
+				    internal-time-units-per-second)
+				 ,maxtime)
+			      (sb-thread:thread-alive-p ,thread))
+			 (progn
+			   (sb-thread:terminate-thread ,thread)
+			   (push ',(cadr expr) *runtime-error*)
+			   (setf ,res 'runtime-error)))
+			 ((sb-thread:thread-alive-p ,thread) (,keep-time ,stime))
+			 (t ,res))))
+	 (,keep-time (get-internal-real-time))))))
+
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect
+	     `(report-result (time-execution
+			      (handler-case ,f
+				(error (condition)
+				  (push condition *runtime-error*)
+				  condition))
+			      ,*max-time*) ',f))))
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
